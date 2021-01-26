8f45a8d8ab706b03a91fae66dd8346a216409b80
Oscar-02 <oscacanas29@gmail.com>
2021-01-25 22:34:58 -0600

    0.2 Basic-Functions-Update
    Añadido funciones basicas del ejercicio

--- G3E2/Program.cs
+++ G3E2/Program.cs
@@ -6,7 +6,18 @@ namespace G3E2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaro las variables
+            int b = 0;
+            int h = 0;
+            int a = 0;
+            Console.WriteLine("Por favor ingrese el valor de la base del triangulo");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bien, ahora por favor ingrese el valor de la altura del triangulo");
+            h = Convert.ToInt32(Console.ReadLine());
+            a = (b * h) / 2;
+            Console.WriteLine("El area de ese triangulo es de: " + a);
+            Console.WriteLine("\nIngrese cualquier tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
